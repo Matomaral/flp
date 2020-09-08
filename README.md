0fcb768653cb10ce674fab716f3f3f4024f7e683
rojbar <santiago.rojas.barreneche@gmail.com>
2020-09-08 15:00:10 -0500

    Se añade gitignore, Puntos Taller1 2,5,8,11,16

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-FLP 
+FLP
+
+Santiago Rojas Barreneche 1841765-3743
+Mateo Obando Gutierrez 1844983-3743 
--- talleres/taller1.rkt
+++ talleres/taller1.rkt
@@ -1,4 +1,22 @@
 #lang eopl
+;; Punto 2
+;; list-tails :
+;; Proposito:
+;; retornar en una lista todas las
+;;sublistas de los elementos consecutivos de la lista
+;;
+(define list-tails
+(lambda (L)
+(if (null? L)empty
+(cons (cons(car L)(cdr L)) (list-tails (cdr L)))     
+)))
+
+;; Pruebas
+(list-tails '(1 2 3 4 5))
+(list-tails '(1 a (e 4) 5 v))
+(list-tails '(a b c d e f g))
+(list-tails '(7 8 9 10 11 9))
+
 ;; Punto 3
 ;; sublist :
 ;; Proposito:
@@ -16,6 +34,44 @@
 ;; Pruebas
 (sublist '(a b c d e) 1 3)
 (sublist '((a b) c a b c 9) 3 4)
+
+;; Punto 5
+;; auxsuma-fibo
+;; crea la sucesion de fibonacci
+
+(define auxsuma-fibo
+  (lambda (n)
+    (cond
+      [(= n 0) 0]
+      [(= n 1) 1]
+      [else (+ (auxsuma-fibo (- n 1))(auxsuma-fibo (- n 2)))]
+      )))
+
+;; auxlista-fibo
+;; crea una lista con cada numero de fibonacci
+
+(define auxlista-fibo
+  (lambda (n)
+    (if (>= n 0)
+        (cons (auxsuma-fibo n) (auxlista-fibo (- n 1)))empty
+        ))             
+  )
+
+;; list-fibo :
+;; Proposito:
+;; recibe como argumento un numero entero n,
+;; y retorna una lista ascendente con los n-terminos de la sucesion Fibonacci.
+
+
+(define list-fibo
+  (lambda (n)
+    (reverse (auxlista-fibo n))))
+
+;; Pruebas
+(list-fibo 6)
+(list-fibo 1)
+(list-fibo 9)
+(list-fibo 20)
 
 ;; Punto 6
 ;; aux-factorial:
@@ -69,6 +125,25 @@
 (list-facts-two 5)
 (list-facts-two 8)
 
+;; Punto 8
+;; flatten :
+;; Proposito:
+;; Recibe como argumento una lista L y devuelve la lista eliminando los parentesis internos
+
+
+(define (flatten L)
+  (cond ((null? L) '())
+        ((pair? L)
+         (append (flatten (car L)) (flatten (cdr L))))
+        (else (list L))))
+
+;;Pruebas
+
+(flatten '((a b) c (((d)) e)))
+(flatten '((a) () (2 ()) () (c)))
+(flatten '((a b c  (d e f)())))
+(flatten '((a b c (a)(e i 2)(3)  (d e f)())))
+
 ;; Punto 9:
 ;; every?:
 ;; Proposito:
@@ -86,6 +161,25 @@
 
 (every? symbol? '(a b c 3 e))
 (every? number? '(1 2 3 5 4))
+
+;; Punto 11
+;; merge
+;; Proposito: retorna una lista ordenada de todos los elementos de las listas L1 y L2.
+
+(define (merge L1 L2)
+  (cond
+    [(null? L1) L2]    
+    [(null? L2) L1]    
+    [(< (car L1) (car L2))    
+     (cons (car L1) (merge (cdr L1) L2))]     
+    [else (cons (car L2) (merge L1 (cdr L2)))]))
+     
+;; PRUEBAS
+
+(merge '(1 4) '(1 2 8))
+(merge '(1 4) '())
+(merge '() '(1 2 8))
+(merge '(35 62 81 90 91) '(3 83 85 90))
 
 ;; Punto 12
 ;; zip:
@@ -122,6 +216,41 @@
 ;; Pruebas
 (hermite 5 2)
 (hermite 5 8)
+
+;; Punto 16
+;; FUNCIONES AUXILIARES
+;; bubble-algorithm
+;; La funcion ejecuta el algoritmo bubble-sort con los pasos correspondientes explicados a continuación.
+
+(define (bubble-algorithm L)
+    (if (null? (cdr L))L  ;; Si no hay elementos a la derecha para comparar, se retorna la lista como está.             
+        (if (< (car L) (cadr L)) ;; Se toma el primer elemento y se compara si es menor que el siguiente
+            (cons (car L) (bubble-algorithm (cdr L))) ;; en este caso es menor, por lo tanto se deja el elemento en la posicion que esta y se va creando la lista ordenada                 
+            (cons (cadr L) (bubble-algorithm (cons (car L) (cddr L))))))) ;; en este caso es mayor, por lo tanto con un pequeño algoritmo utilzando el manejo de listas intercambiamos de posicion los dos elementos comparados.
+;;con este algoritmo hemos puesto el cadr(elemento de la derecha) de primero y con el cons car y cdddr hemos dejado el elemento de la izquierda al final con el resto de la lista que no se ha comparado.
+
+;;bubble-sort-aux
+;; Se necesita una funcion auxiliar que lleve un conteo de cuantas pasadas se llevan, ya que para que el algoritmo
+;; sea correcto se deben realizar n-1 pasadas dependiendo del tamaño de la lista, nuestro primer algoritmo no se ejecutaba las veces necesarias recursivamente.
+
+(define (bubble-sort-aux tamano Lista)    
+    (cond ((= tamano 1) (bubble-algorithm Lista))   
+          (else (bubble-sort-aux (- tamano 1) (bubble-algorithm Lista)))))
+
+
+;; bubble-sort
+;; Proposito:
+;; recibe como entrada una lista de numeros L y retorna la lista L ordenada de manera ascendente.
+
+(define (bubble-sort L) 
+    (bubble-sort-aux (length L) L))
+
+
+;; Pruebas
+(bubble-sort '(5 10 9 8 7))
+(bubble-sort '(8 2 1 6 8))
+(bubble-sort '(8 2 5 2 3))
+(bubble-sort '(9 8 7 6 -1 5 -4))
 
 ;; Punto 17
 ;; aux-path:
